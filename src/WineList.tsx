37b7797a8179d53363d6603ab96be0358cb35563
AgataTalita <startingcommunity@protonmail.com>
2023-12-13 00:50:24 +0100

    Clean up the WineList component

--- src/WineList.tsx
+++ src/WineList.tsx
@@ -5,66 +5,75 @@ import { FilterPanel } from "./FilterPanel";
 import { WineItemSkeleton } from "./WineItemSkeleton";
 import styles from "./WineList.module.scss";
 
-const fetchWines = async (filterOptions: any) => {
-  let countryCodes = "q=pugliadfd";
-  if (filterOptions.length > 0)
-    countryCodes = filterOptions
-      .map((code: string) => `country_codes[]=${code}`)
-      .join("&");
+const fetchWines = async (filterOptions: string[]) => {
+  let params = new URLSearchParams();
+  const baseQueryString = "puglia";
 
-  console.log(countryCodes);
-  const response = await fetch(
-    "https://corsproxy.io/?" +
-      encodeURIComponent(
-        `https://api.vivino.com/vintages/_explore?limit=50&${countryCodes}`
-      )
-  );
-  const wines = (await response.json()).matches;
-  return wines;
+  if (filterOptions.length > 0) {
+    filterOptions.forEach((code) => {
+      params.append("country_codes[]", code);
+    });
+  } else {
+    params.append("q", baseQueryString);
+  }
+
+  const url = `https://corsproxy.io/?${encodeURIComponent(
+    `https://api.vivino.com/vintages/_explore?limit=50&${params.toString()}`
+  )}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch wines");
+  }
+  return (await response.json()).matches;
 };
 
 const WineList: React.FC = () => {
   const [filterOptions, setFilterOptions] = useState<string[]>([]);
-  const { data, isLoading } = useQuery(["wines", filterOptions], () =>
+  const { data, isLoading, error } = useQuery(["wines", filterOptions], () =>
     fetchWines(filterOptions)
   );
 
-  const handleFilterChange = (options: any) => {
+  const handleFilterChange = (options: string[]) => {
     setFilterOptions(options);
   };
 
-  return (
-    <div style={{ width: "100%" }} className={styles.wineListContainer}>
-      <FilterPanel onChange={handleFilterChange} />
-      {isLoading ? (
-        <ul>
-          <li>
-            <WineItemSkeleton />
-          </li>
-          <li>
-            <WineItemSkeleton />
-          </li>
-          <li>
-            <WineItemSkeleton />
-          </li>
-        </ul>
-      ) : data.length > 0 ? (
-        <ul>
-          {data?.map((wine: any, index: number) => (
-            <li key={wine.vintage.id}>
-              <WineItem wine={wine} />
-            </li>
-          ))}
-        </ul>
-      ) : (
+  const renderWineList = () => {
+    if (isLoading)
+      return (
+        <>
+          <WineItemSkeleton />
+          <WineItemSkeleton />
+          <WineItemSkeleton />
+        </>
+      );
+    if (error)
+      return (
         <div className={styles.emptyListText}>
-          <p>No wines found</p>
-          <div>
-            No wines matched your search. Please adjust your filters or check
-            back with us later for more exquisite selections.
-          </div>
+          Error loading wines. Please try again later.
         </div>
-      )}
+      );
+    if (data && data.length > 0)
+      return data.map((wine: any, index: number) => (
+        <li key={wine.vintage.id}>
+          <WineItem wine={wine} />
+        </li>
+      ));
+    return (
+      <div className={styles.emptyListText}>
+        <p>No wines found</p>
+        <div>
+          No wines matched your search. Please adjust your filters or check back
+          with us later for more exquisite selections.
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.wineListContainer}>
+      <FilterPanel onChange={handleFilterChange} />
+      <ul>{renderWineList()}</ul>
     </div>
   );
 };
