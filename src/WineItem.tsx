08da6b9a3372e53d32bfbc1948e2ad12e7587808
AgataTalita <startingcommunity@protonmail.com>
2023-12-12 13:35:40 +0100

    Move WineItem to a separate file

---
+++ src/WineItem.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./WineItem.module.scss";
+import { CircleFlag } from "react-circle-flags";
+import Rating from "./Rating";
+
+interface WineType {
+  vintage: {
+    wine: any;
+    image: any;
+    name: string;
+  };
+}
+
+interface WineItemProps {
+  wine: WineType;
+}
+
+export const WineItem: React.FC<WineItemProps> = ({ wine }) => {
+  return (
+    <div className={styles.wineItem}>
+      <div
+        style={{
+          backgroundImage: `url(${wine.vintage.image.variations.bottle_small})`,
+        }}
+        className={styles.wineImage}
+      ></div>
+      <div className={styles.wineText}>
+        <div className={styles.winery}>{wine.vintage.wine.winery.name}</div>
+        <div className={styles.name}>{wine.vintage.wine.name}</div>
+        <div className={styles.origin}>
+          <CircleFlag
+            countryCode={wine.vintage.wine.region.country}
+            height="24"
+          />
+          <span className={styles.region}>{wine.vintage.wine.region.name}</span>
+        </div>
+      </div>
+      <div className={styles.rating}>
+        <Rating
+          value={wine.vintage.wine.statistics.ratings_average}
+          count={wine.vintage.wine.statistics.ratings_count}
+        />
+      </div>
+    </div>
+  );
+};
--- src/WineList.tsx
+++ src/WineList.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { useQuery } from "react-query";
-import styles from "./WineList.module.scss";
-import { CircleFlag } from "react-circle-flags";
-import Rating from "./Rating";
-
-interface Wine {
-  vintage: {
-    name: string;
-  };
-}
+import { WineItem } from "./WineItem";
 
 const fetchWines = async () => {
   const response = await fetch(
@@ -29,35 +21,7 @@ const WineList: React.FC = () => {
       <ul>
         {data?.map((wine: any, index: number) => (
           <li key={wine.vintage.id}>
-            <div className={styles.wineItem}>
-              <div
-                style={{
-                  backgroundImage: `url(${wine.vintage.image.variations.bottle_small})`,
-                }}
-                className={styles.wineImage}
-              ></div>
-              <div className={styles.wineText}>
-                <div className={styles.winery}>
-                  {wine.vintage.wine.winery.name}
-                </div>
-                <div className={styles.name}>{wine.vintage.wine.name}</div>
-                <div className={styles.origin}>
-                  <CircleFlag
-                    countryCode={wine.vintage.wine.region.country}
-                    height="24"
-                  />
-                  <span className={styles.region}>
-                    {wine.vintage.wine.region.name}
-                  </span>
-                </div>
-              </div>
-              <div className={styles.rating}>
-                <Rating
-                  value={wine.vintage.wine.statistics.ratings_average}
-                  count={wine.vintage.wine.statistics.ratings_count}
-                />
-              </div>
-            </div>
+            <WineItem wine={wine} />
           </li>
         ))}
       </ul>
